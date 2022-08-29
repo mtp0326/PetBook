05e3cba9c57f96439c7eee24e814b21970bb1d20
Andreas Haeberlen <ahae@cis.upenn.edu>
2022-08-29 08:12:43 -0400

    Added HW0

---
+++ HW0/hello-world.js
@@ -0,0 +1,8 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  // TODO: Change the text below to 'Hello World - this is <yourName> (<yourSEASlogin>)'
+  // Example: 'Hello World - this is Andreas Haeberlen (ahae)'
+  res.end('You need to edit hello-world.js!\n');
+}).listen(8080, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:8080/');
