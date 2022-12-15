6a4c7913e0b2e392beaebf1211e2bcbe98d9bbf1
Alex Park <61362943+bluecoap@users.noreply.github.com>
2022-12-15 15:56:23 -0500

    Create visualizerRoutes.js

---
+++ routes/visualizerRoutes.js
@@ -0,0 +1,70 @@
+var db = require('../models/database.js');
+
+var getVisualizer = function (req, res) {
+	if (!req.session.username) {
+ 		return res.redirect('/');
+	}
+  	res.render('friendvisualizer.ejs');
+}
+
+// Gets a list of friends and users with the same affiliation for the visualizer
+var getFriendsVisualize = function(req, res) {
+  	if (!req.session.username) {
+    	return res.redirect('/');
+  	} else {
+		db.getFriends(req.session.username, function (err1, data1) {
+			if (err1) {
+				console.log(err1);
+			} else {
+				var node = {"id": req.session.username, 
+				            "name": "Me", 
+				            "children": []};
+				data1.forEach(function (r) {
+					node.children.push({
+						"id": r, 
+			            "name": r, 
+			            "children": [],
+					});
+				});
+				res.send(node);
+			}
+		});
+	}
+}
+
+// When user clicks on a node, that user's friends will appear
+var expandGraph = function(req, res) {
+  	if (!req.session.username) {
+    	return res.redirect('/');
+  	} else {
+		var clickedFriend = req.params.user;	
+		db.getFriends(clickedFriend, function(err, data) {
+			if (err) {
+				console.log(err);
+			} else {
+				var node = {
+					"id": clickedFriend, 
+		            "name": clickedFriend, 
+		            "children": [], 
+				}
+				//console.log("here: "+ JSON.stringify(data));
+				data.forEach(function (r) {
+					node.children.push({
+						"id": r, 
+			            "name": r, 
+			            "children": [], 
+					});
+				});
+				res.send(node);
+			}
+		});
+	}
+}
+
+var routes = {
+  get_friend_visualizer: getVisualizer,
+  get_friends: getFriendsVisualize,
+  get_more_friends: expandGraph,
+};
+
+module.exports = routes;
