085514cacc39453bef29576dab3b3ec6edf24c0a
sarahzhanng <120423335+sarahzhanng@users.noreply.github.com>
2022-12-16 01:23:13 +0000

    Add files via upload

---
+++ project/src/main/java/edu/upenn/cis/nets2120/config/Config.java
@@ -0,0 +1,7 @@
+package edu.upenn.cis.nets2120.config;
+
+public class Config {
+	public static Boolean LOCAL_DB = false;
+	public static String NEWS_PATH = "http://penn-cis545-files.s3.amazonaws.com/News_Category_Dataset_v2.json";
+	public static int PARTITIONS = 5;
+}
